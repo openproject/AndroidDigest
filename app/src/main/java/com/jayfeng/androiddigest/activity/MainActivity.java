3f7d8548dc4df4bf9ab11ac181bf70463ad0edee
Jay <673592063@qq.com>
2015-04-08 22:56:19 +0800

    add tab more that can extend a feature list later

--- app/src/main/java/com/jayfeng/androiddigest/activity/MainActivity.java
+++ app/src/main/java/com/jayfeng/androiddigest/activity/MainActivity.java
@@ -5,7 +5,11 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.RadioButton;
 
@@ -16,7 +20,8 @@ import com.jayfeng.androiddigest.fragment.OfflineFragment;
 import com.jayfeng.lesscode.core.ViewLess;
 
 
-public class MainActivity extends ActionBarActivity implements RadioButton.OnCheckedChangeListener {
+public class MainActivity extends ActionBarActivity
+        implements RadioButton.OnCheckedChangeListener, View.OnClickListener {
 
     private Toolbar toolbar;
     private FragmentManager fragmentManager;
@@ -24,6 +29,7 @@ public class MainActivity extends ActionBarActivity implements RadioButton.OnChe
     private RadioButton homeTabBtn;
     private RadioButton blogTabBtn;
     private RadioButton toolTabBtn;
+    private Button moreTabBtn;
 
     private Fragment currentFragment;
     private Fragment homeFragment;
@@ -49,10 +55,12 @@ public class MainActivity extends ActionBarActivity implements RadioButton.OnChe
         homeTabBtn = ViewLess.$(this, R.id.tab_home_btn);
         blogTabBtn = ViewLess.$(this, R.id.tab_blog_btn);
         toolTabBtn = ViewLess.$(this, R.id.tab_tool_btn);
+        moreTabBtn = ViewLess.$(this, R.id.tab_more_btn);
 
         homeTabBtn.setOnCheckedChangeListener(this);
         blogTabBtn.setOnCheckedChangeListener(this);
         toolTabBtn.setOnCheckedChangeListener(this);
+        moreTabBtn.setOnClickListener(this);
 
         currentFragment = homeFragment = new HomeFragment();
         blogFragment = new BlogFragment();
@@ -99,6 +107,28 @@ public class MainActivity extends ActionBarActivity implements RadioButton.OnChe
                     break;
             }
         }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.tab_more_btn:
+                popupMoreMenu();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void popupMoreMenu() {
+        PopupMenu popup = new PopupMenu(this, moreTabBtn);
+        popup.getMenuInflater().inflate(R.menu.more_popup_menu, popup.getMenu());
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            public boolean onMenuItemClick(MenuItem item) {
+                return true;
+            }
+        });
+        popup.show();
     }
 
 }
